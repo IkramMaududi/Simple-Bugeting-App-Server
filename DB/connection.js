95dc066c4fd8b666d6b3c7c38eeed3a754adc5c4
IkramMaududi <onime.kyo999@gmail.com>
2021-06-24 21:42:03 +0700

    first try locally

---
+++ DB/connection.js
@@ -0,0 +1,22 @@
+const {Pool} = require("pg");
+
+//* this is for local environment, otherwise comment this below
+const pool = new Pool ({
+    user: process.env.USER_PSQL,
+    password: process.env.PASSWORD_PSQL,
+    host: process.env.HOST_PSQL,
+    port: process.env.PORT_PSQL,
+    database: process.env.DATABASE_PSQL
+});
+
+
+
+// //* this is for production environment, otherwise comment this below
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+module.exports = pool;
---
+++ routes/User.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const pool = require('../db/connection')
+
+const router = express.Router();
+
+router
+    .route('/')
+    .get( (req,res) => {
+        res.send('hello get /user/');
+    });
+
+router
+    .route('/addition')
+    .post( async (req,res) => {
+        try {
+            const { actionType, inputDate, inputDetail, inputAmount, inputUsername } = req.body;
+            const client = await pool.connect();
+            // console.log(username);
+
+            // Check whether that specific username exist, if it doesn't exist make a new one, else leave it be 
+            const userExist = await client.query(
+                "SELECT * FROM users WHERE username = $1;",
+                [inputUsername]
+            );
+            if ( !userExist.rowCount ) {
+                const enterNewUser = await client.query(
+                    "INSERT INTO users (username) VALUES $1;",
+                    [inputUsername]
+                );
+            };
+
+            if (actionType === 'income') {
+                const newInsert = await client.query(
+                    "INSERT INTO income (username, submitted_date, item_name, amount) VALUES ($1, $2, $3, $4;",
+                    [inputUsername, inputDate, inputDetail, inputAmount]
+                );
+                res.json({
+                    working: true,
+                    message: 'an income added'
+                });
+            } else if (actionType === 'expense') {
+                const newInsert = await client.query(
+                    "INSERT INTO expense (username, submitted_date, item_name, amount) VALUES ($1, $2, $3, $4;",
+                    [inputUsername, inputDate, inputDetail, inputAmount]
+                );
+                res.json({
+                    working: true,
+                    message: 'an expense added'
+                });
+            };
+            client.release();
+        } catch (err) {
+            res.status(400).send({error: err.message}); 
+        };
+    });
+
+router
+    .route('/showall')
+    .get( async (req,res) => {
+        try {
+            const {inputUsername} = req.body;
+            const client = await pool.connect();
+
+            const userIncome = await client.query(
+                "SELECT * FROM income WHERE username = $1 ORDER BY submitted_date;", 
+                [inputUsername]
+            );
+            const userExpense = await client.query(
+                "SELECT * FROM expense WHERE username = $1 ORDER BY submitted_date;", 
+                [inputUsername]
+            );
+
+            // send back the data to front end through json here and sort it
+            // if the userIncome or userExpense exist, send them, else just give null value
+            let income = null, 
+                expense = null;
+            if (userIncome) {
+                income = userIncome.rows;
+            };
+            if (userExpense) {
+                expense = userExpense.rows
+            };
+            
+            res.send({
+                dataIncome = income,
+                dataExpense: expense
+            });
+
+
+            client.release();
+        } catch (err) {
+            res.status(400).send({ error: err.message}); 
+        };
+    });
